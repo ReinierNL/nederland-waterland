485c1a9913c0149c12ece71db31113d2a5ada5df
Erik Vullings <erik.vullings@gmail.com>
2021-02-26 17:22:02 +0100

    Only show when properties are defined

--- src/components/info-panel.ts
+++ src/components/info-panel.ts
@@ -18,21 +18,24 @@ export const InfoPanel: MeiosisComponent = () => {
       const props = selectedItem && selectedItem.properties;
       const propStyle = selectedLayer && propertyStyles[selectedLayer];
       const properties = propStyle && propStyle.properties;
-      return m('.info-panel', [
-        // m('p', space),   // attempted to add some space between previous content and organisation and this panel
-        selectedLayer && m('h4.title', `Geselecteerd: ${layerTitles[selectedLayer] || selectedLayer}`),
-        props &&
-          m('table.feature-props', [
-            ...Object.keys(props)
-              .filter((key) => !properties || properties.hasOwnProperty(key))
-              .map((key) =>
-                m('tr', [
-                  m('td.bold.toright', !properties ? key : properties[key].title(key)),
-                  m('td', !properties ? props[key] : properties[key].value(props[key])),
-                ])
-              ),
-          ]),
-      ]);
+      return (
+        properties &&
+        m('.info-panel', [
+          // m('p', space),   // attempted to add some space between previous content and organisation and this panel
+          selectedLayer && m('h4.title', `Geselecteerd: ${layerTitles[selectedLayer] || selectedLayer}`),
+          props &&
+            m('table.feature-props', [
+              ...Object.keys(props)
+                .filter((key) => !properties || properties.hasOwnProperty(key))
+                .map((key) =>
+                  m('tr', [
+                    m('td.bold.toright', !properties ? key : properties[key].title(key)),
+                    m('td', !properties ? props[key] : properties[key].value(props[key])),
+                  ])
+                ),
+            ]),
+        ])
+      );
     },
   };
 };
