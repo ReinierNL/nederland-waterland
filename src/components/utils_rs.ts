63a7e440c3a8ba947c4d7c8802a983ed29809123
Reinier Sterkenburg <reinier.sterkenburg@tno.nl>
2021-05-21 07:22:58 +0200

    Replaced isInstellingLayer with IsCareLayer, isCureLayer and isCareOrCureLayer

--- src/components/utils_rs.ts
+++ src/components/utils_rs.ts
@@ -1,5 +1,13 @@
 // export function(s) that can be used from different places
 
-export const isInstellingLayer = (layername: string): boolean => {
-  return layername == 'ziekenhuizen' || layername == 'vvt' || layername == 'ghz' || layername == 'ggz'
+export const isCareLayer = (layername: string): boolean => {
+  return layername == 'vvt' || layername == 'ghz' || layername == 'ggz'
+}
+
+export const isCureLayer = (layername: string): boolean => {
+  return layername == 'ziekenhuizen' || layername == 'poliklinieken'
+}
+
+export const isCareOrCureLayer = (layername: string): boolean => {
+  return isCareLayer(layername) || isCureLayer(layername)
 }
