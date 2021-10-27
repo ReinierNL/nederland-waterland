0df0ed1f80eb49ea5fec6c1b902589325911f98e
Erik Vullings <erik.vullings@gmail.com>
2021-10-27 12:27:47 +0200

    Data specified in home-page

--- src/components/chart-js.ts
+++ src/components/chart-js.ts
@@ -25,6 +25,10 @@ import {
   Title,
   Tooltip,
   SubTitle,
+  ChartConfiguration,
+  ChartTypeRegistry,
+  ScatterDataPoint,
+  BubbleDataPoint,
 } from 'chart.js';
 
 Chart.register(
@@ -57,8 +61,7 @@ Chart.register(
 export const ChartJs: FactoryComponent<{
   width?: number;
   height?: number;
-  data?: Record<string, unknown>;
-  options?: Record<string, unknown>;
+  data: ChartConfiguration<keyof ChartTypeRegistry, (number | ScatterDataPoint | BubbleDataPoint | null)[], unknown>;
 }> = () => {
   let canvas: HTMLCanvasElement;
   let chart: Chart;
@@ -67,44 +70,9 @@ export const ChartJs: FactoryComponent<{
     view: ({ attrs: { width = 600, height = 400 } }) => {
       return m(`canvas.pie-chart[width=${width}][height=${height}]`);
     },
-    oncreate: ({ dom }) => {
+    oncreate: ({ dom, attrs: { data } }) => {
       canvas = dom as HTMLCanvasElement;
-      chart = new Chart(canvas, {
-        type: 'bar',
-        data: {
-          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-          datasets: [
-            {
-              label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-              ],
-              borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-              ],
-              borderWidth: 1,
-            },
-          ],
-        },
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true,
-            },
-          },
-        },
-      });
+      chart = new Chart(canvas, data);
     },
   };
 };
