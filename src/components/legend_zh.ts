a6779a05d9923ac6cfde4127c939c6e13f0ad52a
Reinier Sterkenburg <reinier.sterkenburg@tno.nl>
2021-12-18 20:57:38 +0100

    Improved layout

--- src/components/legend_zh.ts
+++ src/components/legend_zh.ts
@@ -15,13 +15,13 @@ export const Legend_zh: MeiosisComponent = () => {
       const legend = propertyStyle && propertyStyle.legend;
       return (
         legend && m('.legend', [
-          m('h5', { style: 'margin-bottom: 0;' }, `Legenda ${legend.title}`),
+          m('h5', { style: 'margin-bottom: 10;' }, `Legenda ${legend.title}`),
           legend.items.map((item) =>
             m('.legend-item', [
               m('.legend-circle', {
                 style: `background-color: ${item[2]}`,
               }),
-              m('span', item[3]),
+              m('.legend-circle-item', item[3]),
             ])
           ),
         ])
