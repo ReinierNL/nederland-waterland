daceb800a96887d2ddec6c660ed4b3abcf757188
Reinier Sterkenburg <reinier.sterkenburg@tno.nl>
2022-02-13 13:33:15 +0100

    Added containsTEOLayer(); moved activeLayersAsString

--- src/components/utils_rs.ts
+++ src/components/utils_rs.ts
@@ -1,59 +1,65 @@
 // export function(s) that can be used from different places
 
-export const isCareLayer = (layername: string): boolean => {
-  return layername == 'vvt' || layername == 'ghz' || layername == 'ggz'
-}
-
-export const isCategoralLayer = (layername: string): boolean => {
-  return layername == 'categorale_instellingen'
-}
-
-export const isCureLayer = (layername: string): boolean => {
-  return layername == 'ziekenhuizen' || layername == 'poliklinieken' || layername == 'categorale_instellingen'
-}
-
-export const isCareOrCureLayer = (layername: string): boolean => {
-  return isCareLayer(layername) || isCategoralLayer(layername) || isCureLayer(layername)
-}
-
-export const isDeltaresLayer = (layername: string): boolean => {
-  return layername == 'wateren_potentie_gt1ha'
-}
-
-export const isEnergyRelatedLayer = (layername: string): boolean => {
-  return !isCareOrCureLayer(layername) && !isSportLayer(layername)
-}
-
-export const isSportLayer = (layername: string): boolean => {
-  return layername == 'skating' || layername == 'swimming'
-}
-
-export const isSyntraalLayer = (layername: string): boolean => {
-  return layername == 'rwzis' || layername == 'effluent' || layername == 'rioolleidingen'
-}
-
-export const isTEOLayer = (layername: string): boolean => {
-  return layername.substring(0, 8) == 'wateren_'
-}
-
-export const isTVWLayer = (layername: string): boolean => {
-  return layername == 'tvw'
-}
-
-export const isVattenfallLayer = (layername: string): boolean => {
-  return layername.substring(0, 5) == 'wn_vf'
-}
-
-export const isWKOLayer = (layername: string): boolean => {
-  return layername.substring(0, 4) == 'wko_'
-}
-
 export const activeLayersAsString = (activeLayers: Set<string>): string => {
   var s = '';
   activeLayers.forEach(
-    function(item){
+    function(item) {
       s = s + item + ','
     }
   );
   return s
 }
+
+export const containsTEOLayer = (activeLayers: Set<string>): boolean => {
+  const sActiveLayers = activeLayersAsString(activeLayers);
+  return sActiveLayers!.includes('wateren_')
+}
+
+export const isCareLayer = (layerName: string): boolean => {
+  return layerName == 'vvt' || layerName == 'ghz' || layerName == 'ggz'
+}
+
+export const isCategoralLayer = (layerName: string): boolean => {
+  return layerName == 'categorale_instellingen'
+}
+
+export const isCureLayer = (layerName: string): boolean => {
+  return layerName == 'ziekenhuizen' || layerName == 'poliklinieken' || layerName == 'categorale_instellingen'
+}
+
+export const isCareOrCureLayer = (layerName: string): boolean => {
+  return isCareLayer(layerName) || isCategoralLayer(layerName) || isCureLayer(layerName)
+}
+
+export const isDeltaresLayer = (layerName: string): boolean => {
+  return layerName == 'wateren_potentie_gt1ha'
+}
+
+export const isEnergyRelatedLayer = (layerName: string): boolean => {
+  return !isCareOrCureLayer(layerName) && !isSportLayer(layerName)
+}
+
+export const isSportLayer = (layerName: string): boolean => {
+  return layerName == 'skating' || layerName == 'swimming'
+}
+
+export const isSyntraalLayer = (layerName: string): boolean => {
+  return layerName == 'rwzis' || layerName == 'effluent' || layerName == 'rioolleidingen'
+}
+
+export const isTEOLayer = (layerName: string): boolean => {
+  return layerName.substring(0, 8) == 'wateren_'
+}
+
+export const isTVWLayer = (layerName: string): boolean => {
+  return layerName == 'tvw'
+}
+
+export const isVattenfallLayer = (layerName: string): boolean => {
+  return layerName.substring(0, 5) == 'wn_vf'
+}
+
+export const isWKOLayer = (layerName: string): boolean => {
+  return layerName.substring(0, 4) == 'wko_'
+}
+
